1408a33d6350374d8695b5f2d7fd5a300f9026bb
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2022-07-08 18:32:19 +0100

    comission

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for v in a_dictionary.values:
+    for v in a_dictionary.values():
         if v == value:
-            k = list(a_dictionary.keys())[list(a_dictionary.values()).index(v)]
-            del a_dictionary[k] 
+            del a_dictionary[list(a_dictionary.keys())[list(a_dictionary.values()).index(v)]]
