a9daf3d304db617809a70b5d20bdb2117028cc58
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2022-07-29 20:39:05 +0100

    committee

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,4 +18,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in allowed_param_types:
         raise TypeError("b must be an integer")
+        
     return int(a) + int(b)
